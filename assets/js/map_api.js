3daba934fab9b5f0d480b7b3bdea840e5dd3c7fb
Hardik Rola <hrsautomation20@gmail.com>
2021-09-14 17:20:34 -0400

    Updated map_api.js and index.html

---
+++ assets/js/map_api.js
@@ -0,0 +1,23 @@
+var origin = "Cary, NC";
+
+window.onload = function () {
+  L.mapquest.key = "MxMEt0lAXnEnzLPH7q3pPeMkwmaa422h";
+
+  L.mapquest.geocoding().geocode(origin, createMap);
+
+  function createMap(error, response) {
+    var location = response.results[0].locations[0];
+    var latLng = location.displayLatLng;
+    var map = L.mapquest.map("map", {
+      center: latLng,
+      layers: L.mapquest.tileLayer("map"),
+      zoom: 14,
+    });
+
+    var customIcon = L.mapquest.icons.circle({
+      primaryColor: "#3b5998",
+    });
+
+    L.marker(latLng, { icon: customIcon }).addTo(map);
+  }
+};
