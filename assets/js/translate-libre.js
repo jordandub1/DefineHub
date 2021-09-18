32e4d41fe603e106f6a0beb3513e8b5298c343da
Hardik Rola <hrsautomation20@gmail.com>
2021-09-17 23:06:36 -0400

    Updated translator js file

--- assets/js/translate-libre.js
+++ assets/js/translate-libre.js
@@ -1,55 +1,40 @@
-var wordToTranslate = "book";
+var wordToTranslate = "Excellence";
 var sourceLanguage = "en";
-var targetLanguage = "fr";
+// var targetLanguage = "fr";
+var targetLanguage = document.getElementById("dropdown");
 
-
-fetch("https://libretranslate.de/translate?q=" + wordToTranslate + 
-    "&source=" + sourceLanguage + 
-    "&target=" + targetLanguage + "&format=text&api_key=xxxxxxxx-xxx-xxxx-xxxx-xxxxxxxxxxxx", {
-    method:"POST",
-    "headers": {
-        "accept": "application/json",
-        "Content-Type": "application/x-www-form-urlencoded"
+//On Dropdown change word will be translated to the choosen language
+targetLanguage.addEventListener("change", function (event) {
+  console.log(event.target.value);
+  var targetLanguage1 =
+    targetLanguage.options[targetLanguage.selectedIndex].value;
+  fetch(
+    "https://libretranslate.de/translate?q=" +
+      wordToTranslate +
+      "&source=" +
+      sourceLanguage +
+      "&target=" +
+      targetLanguage1 +
+      "&format=text&api_key=xxxxxxxx-xxx-xxxx-xxxx-xxxxxxxxxxxx",
+    {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
     }
-})
-        .then(response => {
-            console.log(response);
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            console.log(data.translatedText);
-        })
-        .catch(err => {
-            console.error(err);
-            alert(err);
-        });
-    
-// fetch("https://libretranslate.de/translate", {
-//     method: "POST",
-//     body: JSON.stringify({
-//         q: "tree",
-//         source: "en",
-//         target: "es"
-//     }),
-//     headers: { "Content-Type": "application/json" }
-// })
-//     .then(response => {
-//         console.log(response);
-//         return response.JSON();
-//     })
-//     .then(data => {
-//         console.log(data);
-//     });
-
-// const res = fetch("https://libretranslate.de/translate", {
-//         method: "GET",
-//         body: JSON.stringify({
-//             q: "",
-//             source: "en",
-//             target: "es"
-//         }),
-//         headers: { "Content-Type": "application/json" }
-//     });
-    
-//     console.log(res);
+  )
+    .then((response) => {
+      //   console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      //   console.log(data.translatedText);
+      $("h3").text(data.translatedText);
+    })
+    .catch((err) => {
+      console.error(err);
+      alert(err);
+    });
+});
